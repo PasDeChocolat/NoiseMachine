e9e3f428be296bade64fef8494c539bb739de6fb
Kyle Oba <koba@mudphone.com>
2013-05-19 16:57:42 -1000

    Add sound to second layer... need to add pattern.

--- src/grid/sound.clj
+++ src/grid/sound.clj
@@ -1,10 +1,12 @@
 (ns grid.sound
   (:require [overtone.inst.drum :as drum]
             [quil.core :as qc])
-  (:use [grid.setup :only [DEPTH_FAR_THRESH NCOLS NROWS]]))
+  (:use [grid.setup :only [DEPTH_FAR_THRESH LONG_COLS_START_COLS NCOLS NROWS]]))
+;;
 ;; Ideas:
 ;;  - If sector is commonly used, it could have it's volume degrade
 ;;  with use.
+
 (defn- hit-at-dispatch [col row depth]
   (cond
    true :bing
@@ -39,3 +41,23 @@
         decay (qc/constrain-float decay 0.1 1.0)
         ]
     (drum/bing :amp amp :freq freq :attack attack :decay decay)))
+
+(defn rhythm-hit-at-dispatch [col row depth]
+  :bing)
+
+(defmulti rhythm-hit-at
+  "Play the rhythm part of this thing."
+  #'rhythm-hit-at-dispatch
+  :default :bing)
+
+(defmethod rhythm-hit-at :bing [col row depth]
+  (let [freq (qc/map-range col 0 (last LONG_COLS_START_COLS) 120.0 180.0)]
+    (drum/bing :amp 2.0 :freq freq :attack 0.01 :decay 0.5)))
+
+(defmethod rhythm-hit-at :kick [col row depth]
+  (let [freq (qc/map-range col 0 (last LONG_COLS_START_COLS) 200.0 250.0)]
+    (drum/kick :amp 0.00001 :freq freq :attack 0.01 :decay 0.5)))
+
+;; (drum/bing :amp 2.0 :freq 120.0 :attack 0.01 :decay 0.5)
+;; (drum/kick :amp 2.0 :freq 200.0 :attack 0.01 :decay 0.5)
+;; (drum/dance-kick)
